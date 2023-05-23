44dec7644adacd9f89c93e1bbae20ef5e3d90161
murizal555 <murizal444.com>
2023-05-23 18:38:47 +0800

    satu

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+  data(){
+    return{
+      message:'WELCOME TO VUE JS',
+      nama:'murizal',
+      count:0
+    }
+  },
+  methods:{
+    increment(){
+      this.count++
+    }
+  }
+}
+</script>
+<template>
+<h1>{{message}}</h1>
+<p>belajar vue js {{nama}}</p>
+{{nilai}}
+<ul>
+<li style="background:aqua">ayam</li>
+<li style="background:green">ayam</li>
+<li style="background:blue ">ayam</li>
+</ul>
+<button style="background-color:white" @click="increment">hitung mulai = {{count}}</button>
+</template>
---
+++ src/components/icons/testCOM.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+    data(){
+     return{
+        count:0
+     }   
+    }
+}
+</script>
+
+<template>
+<button @click="count++">
+you click me{{count}} times.
+</button>
+</template>
